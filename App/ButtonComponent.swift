a26b4338b5908fe78de9143b765d3883dd8965c7
Dorian Marié <dorian@dorianmarie.com>
2025-04-03 14:43:49 +0200

    nothing

--- App/ButtonComponent.swift
+++ App/ButtonComponent.swift
@@ -16,13 +16,18 @@ class ButtonComponent: BridgeComponent {
         guard let data: MessageData = message.data() else { return }
 
         let viewController = delegate.destination as? UIViewController
-        let action = UIAction(title: data.title) { _ in
+        let action = UIAction() { _ in
             self.reply(to: message.event)
         }
+        
         let button = UIBarButtonItem(primaryAction: action)
-        if let image = data.image {
-            button.image = UIImage(systemName: image)
+
+        if data.image.isEmpty {
+            button.title = data.title
+        } else {
+            button.image = UIImage(systemName: data.image)
         }
+
         viewController?.navigationItem.rightBarButtonItem = button
     }
 
@@ -35,6 +40,6 @@ class ButtonComponent: BridgeComponent {
 private extension ButtonComponent {
     struct MessageData: Decodable {
         let title: String
-        let image: String?
+        let image: String
     }
 }
--- App/TabBarComponent.swift
+++ App/TabBarComponent.swift
@@ -3,19 +3,21 @@ import UIKit
 
 class TabBarComponent: BridgeComponent {
     override class var name: String { "tab-bar" }
-    
+
     override func onReceive(message: Message) {
         if message.event == "connect" {
+
             guard let data: MessageData = message.data() else { return }
-            
+
             let newTabs = data.tabs.map { Tab(title: $0.title, image: $0.image, path: $0.path) }
+
 
             if (newTabs.isEmpty || newTabs == Tab.all) {
                 return
             }
-            
+
             Tab.all = newTabs
-            
+
             let viewController = delegate.destination as? UIViewController
             let tabBarController = viewController?.tabBarController as? TabBarController
             let sceneDelegate = tabBarController?.sceneDelegate as? SceneDelegate
