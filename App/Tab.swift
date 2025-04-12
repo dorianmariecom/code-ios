e5569f5b72d1cca718a229145fde970e01416388
Dorian Marié <dorian@dorianmarie.com>
2025-04-12 14:56:40 +0200

    simpler url

--- App/Tab.swift
+++ App/Tab.swift
@@ -1,5 +1,3 @@
-import Foundation
-
 class Tab: Equatable {
     let title: String
     let image: String
@@ -9,12 +7,12 @@ class Tab: Equatable {
     static var all = [
         Tab(title: "loading...", image: "circle.fill", path: ""),
     ]
- 
+
     init(title: String, image: String, path: String) {
         self.title = title
         self.image = image
         self.path = path
-        self.url = URL(string: "\(AppConfig.baseDomain)/\(path)")!
+        self.url = AppConfig.baseURL.appending(path: tab.path)
     }
 
     static func == (lhs: Tab, rhs: Tab) -> Bool {
